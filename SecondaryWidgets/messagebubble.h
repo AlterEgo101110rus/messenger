ba332bdc3eceeb11126e07cef39457380f16348c
Alexander <131619453+AlterEgo101110rus@users.noreply.github.com>
2026-03-21 00:05:55 +0300

    Implement real reply flow

--- SecondaryWidgets/messagebubble.h
+++ SecondaryWidgets/messagebubble.h
@@ -10,6 +10,7 @@
 #include <QPainter>
 #include <QPainterPath>
 #include <QSvgRenderer>
+#include <QPushButton>
 
 #include "ResourceRegistry.h"
 
@@ -17,11 +18,33 @@ class MessageBubble : public QWidget {
     Q_OBJECT
 
 public:
+    enum BubbleType { Standard, Reply, Forward };
+    enum AttachmentType { NoAttachment, PhotoAttachment, VideoAttachment, FileAttachment, PollAttachment };
+
+    struct MessageData {
+        QString text;
+        BubbleType type = Standard;
+        QString replyPreview;
+        bool replyClickable = false;
+        int replyToMessageId = -1;
+        QString forwardSource;
+        bool forwardSourceClickable = false;
+        AttachmentType attachmentType = NoAttachment;
+        QString attachmentTitle;
+        QString attachmentSubtitle;
+    };
+
     explicit MessageBubble(const QString &text, bool incoming, QWidget *parent = nullptr);
+    explicit MessageBubble(const MessageData &data, bool incoming, QWidget *parent = nullptr);
 
     enum MessageStatus { Sent, Read };
     void setTail(bool show);
     void setStatus(MessageStatus status);
+
+signals:
+    void replyClicked();
+    void forwardSourceClicked();
+    void attachmentClicked();
 
 protected:
     void paintEvent(QPaintEvent *event) override;
@@ -34,6 +57,10 @@ private:
     QLabel *m_timeLabel;
     QLabel *m_statusIconLabel;
 
+    QWidget *createReplyHeader(const MessageData &data);
+    QWidget *createForwardHeader(const MessageData &data);
+    QWidget *createAttachmentWidget(const MessageData &data);
+    QTextEdit *createTextView(const QString &text);
     QPixmap getColoredIcon(const QString &path, const QColor &color, int size);
 };
 
